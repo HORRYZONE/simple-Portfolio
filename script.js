36a37bd2b6716176c35d205883f52c94215dc067
HORRYZONE <danishfitri131@gmail.com>
2023-07-23 07:59:15 +0800

    about section🔧

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-// JavaScript code goes here
 
 const tagNavItems = document.querySelectorAll('.tag-nav-item');
-const cards = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card');
 
-tagNavItems.forEach(item => {
-  item.addEventListener('click', () => {
-    const selectedTag = item.getAttribute('data-tag');
+    tagNavItems.forEach(item => {
+      item.addEventListener('click', () => {
+        const selectedTag = item.getAttribute('data-tag');
 
-    tagNavItems.forEach(item => item.classList.remove('active'));
-    cards.forEach(card => card.style.display = 'none');
+        tagNavItems.forEach(item => item.classList.remove('active'));
+        cards.forEach(card => card.style.display = 'none');
 
-    item.classList.add('active');
+        item.classList.add('active');
 
-    if (selectedTag === 'all') {
-      cards.forEach(card => card.style.display = 'block');
-    } else {
-      const filteredCards = document.querySelectorAll(`[data-tags*="${selectedTag}"]`);
-      filteredCards.forEach(card => card.style.display = 'block');
+        if (selectedTag === 'all') {
+          cards.forEach(card => card.style.display = 'block');
+        } else {
+          const filteredCards = document.querySelectorAll(`[data-tags*="${selectedTag}"]`);
+          filteredCards.forEach(card => card.style.display = 'block');
+        }
+      });
+    });
+
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        const tabId = tab.getAttribute('data-tab');
+
+        tabs.forEach(tab => tab.classList.remove('active'));
+        tabContents.forEach(content => content.classList.remove('active'));
+
+        tab.classList.add('active');
+        document.querySelector(`[data-tab="${tabId}-content"]`).classList.add('active');
+      });
+    });
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Add the typewriter effect to the "About content" section
+  const typewriterElement = document.getElementById('typewriter-container');
+  const aboutText = "Hi, I'm Danish";
+  let typewriterIndex = 0;
+
+  function typewriter() {
+    typewriterElement.textContent = aboutText.slice(0, typewriterIndex);
+    typewriterIndex++;
+
+    if (typewriterIndex <= aboutText.length) {
+      setTimeout(typewriter, 150); // Adjust the typing speed here (in milliseconds)
     }
-  });
-});
+  }
 
-const tabs = document.querySelectorAll('.tab');
-const tabContents = document.querySelectorAll('.tab-content');
+  typewriter();
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', async () => {
-    const tabId = tab.getAttribute('data-tab');
-    
-    tabs.forEach(tab => tab.classList.remove('active'));
-    tabContents.forEach(content => content.style.display = 'none');
-    
-    tab.classList.add('active');
-    
-    try {
-      const response = await fetch(`${tabId}.html`);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch ${tabId} content`);
-      }
-      const tabHTML = await response.text();
-      document.getElementById(`${tabId}-content`).innerHTML = tabHTML;
-      document.getElementById(`${tabId}-content`).style.display = 'block';
-    } catch (error) {
-      console.error(error);
-    }
-  });
 });
 
